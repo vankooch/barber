e4a02fec4a96150905e757edb8aec316b2d71a43
Kiarash Ghiaseddin <kiarash.ghiaseddin@prodot.de>
2019-06-26 15:57:54 +0200

    Better Display attribute handling for schemas
    * Creat a mapping for models using JsonPropertyAttributes and try to match them
    * Also proccess DispalyAttribute if there is no resx file attached to it

--- src/Barber.OpenApi.Extensions/SchemaFilter/DisplaySchemaFilter.cs
+++ src/Barber.OpenApi.Extensions/SchemaFilter/DisplaySchemaFilter.cs
@@ -4,6 +4,7 @@
     using System.ComponentModel.DataAnnotations;
     using System.Linq;
     using Microsoft.OpenApi.Models;
+    using Newtonsoft.Json;
     using Swashbuckle.AspNetCore.SwaggerGen;
 
     /// <summary>
@@ -28,11 +29,26 @@
             if (schema.Properties?.Count > 0)
             {
                 var propertyInfos = context.Type.GetProperties();
+                var dict = propertyInfos
+                        .ToLookup(
+                            e => (JsonPropertyAttribute)e.GetCustomAttributes(typeof(JsonPropertyAttribute), true).FirstOrDefault(),
+                            e => e.Name)
+                        .Where(e => e.Key != null && e.Key != default)
+                        .ToLookup(e => e?.Key?.PropertyName, e => e.FirstOrDefault());
 
                 foreach (var item in schema.Properties)
                 {
                     var name = item.Key[0].ToString().ToUpper() + item.Key.Substring(1);
                     var propertyMatch = propertyInfos.FirstOrDefault(e => e.Name == name);
+                    if (propertyMatch == null)
+                    {
+                        var match = dict.FirstOrDefault(e => e.Key == item.Key)?.FirstOrDefault();
+                        if (!string.IsNullOrEmpty(match))
+                        {
+                            propertyMatch = propertyInfos.FirstOrDefault(e => e.Name == match);
+                        }
+                    }
+
                     if (propertyMatch != null)
                     {
                         var attibutes = propertyMatch.GetCustomAttributes(typeof(DisplayNameAttribute), true);
@@ -50,18 +66,16 @@
                                 continue;
                             }
 
-                            if (attibute.ResourceType != null)
+                            var title = attibute.GetName() ?? attibute.Name;
+                            if (!string.IsNullOrEmpty(title) && !string.IsNullOrWhiteSpace(title))
                             {
-                                item.Value.Title = attibute.GetName();
-                                var description = attibute.GetDescription();
-                                if (!string.IsNullOrEmpty(description) && !string.IsNullOrWhiteSpace(description))
-                                {
-                                    item.Value.Description = description;
-                                }
+                                item.Value.Title = title;
                             }
-                            else
+
+                            var description = attibute.GetDescription() ?? attibute.Description;
+                            if (!string.IsNullOrEmpty(description) && !string.IsNullOrWhiteSpace(description))
                             {
-                                item.Value.Title = attibute.Name;
+                                item.Value.Description = description;
                             }
                         }
                     }
